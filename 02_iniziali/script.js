13bf2fe5e11cc24a3ccb804ce65e61b6768fceb9
MelaniaPicone <melania_picone@yahoo.it>
2025-07-08 21:04:50 +0200

    [add] 02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+//funzione tradizionale
+//function fistLetter(array) {}
+const firstLetter = (array) => {
+const result = [];
+
+for (let i=0; i < array.length ; i++){
+const letter = array[i].charAt(0);
+result.push(letter);
+}
+return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetter(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

3b27b5ecf20b0658dea3242db5505251e3078b0d
MelaniaPicone <melania_picone@yahoo.it>
2025-07-08 21:40:42 +0200

    [add] 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,11 +2,25 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+
+// funzione tradizionale
+// function vocalFind (string) {}
+const letterFind = (string) => {
+
+for (let i=0; i < word.length ; i++){
+const vowelsFind = string[i].match(/aeiou/);
+
+}
+
+return vowelsFind;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(word(vowelsFind));
 
 
 

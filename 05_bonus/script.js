00d43e552a345012a8559f64081f09f6bd473910
MelaniaPicone <melania_picone@yahoo.it>
2025-07-09 10:23:25 +0200

    [add] 05

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,34 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+const greetings = (string) => {
+// variabile contenente la data odierna
+const date = new Date();
+const hours = date.getHours();
+
+let greetings = '';
+
+if (hours < 13) {
+  greetings = 'Buongiorno'
+}
+
+else if (hours < 18) {
+  greetings = 'Buon Pomeriggio'
+}
+
+else {
+  greetings = 'Buonasera'
+}
+
+return `${greetings} ${string}`;
+
+
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(greetings(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
